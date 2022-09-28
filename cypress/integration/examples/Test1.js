a930b0c628fd4584b5feee880c92233ffe9bdb7f
occam_razor <occam_razor@hotmail.com>
2022-09-28 10:11:22 -0700

    New test to add to cart and checkout

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,10 +1,34 @@
 /// <reference types="Cypress" />
 
-describe("My First Test Suite", function () {
+describe("My First Test Suite", function (){
 	it("My FirstTest case", function () {
 		cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
 		cy.get(".search-keyword").type("ca");
+		cy.wait(2000);
 		// To verify all four items are displayed need .visible to discard the invisible 5th element
 		cy.get(".product:visible").should("have.length", 4);
+		// Parent child chaining
+		cy.get('.products').as('productLocator')// This is aliasing 
+		cy.get('@productLocator').find('.product').should('have.length', 4);
+		//cy.get(".products").find(".product").should("have.length", 4); // This is original code without alias
+		cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+		// Finds all four products and iterates thru the array using each($el, index, $list)
+		cy.get(".products").find(".product").each(($el, index, $list) => {
+			const textVeg = $el.find("h4.product-name").text();
+				if (textVeg.includes("Cashews")) {
+					cy.wrap($el).find('button').click()
+				}
+		});
+		//assert if logo text is correctly displayed 
+		cy.get('.brand').should('have.text','GREENKART')
+
+		// this is to print in logs
+		cy.get('.brand').then(function (logoElement) {
+			cy.log(logoElement.text()) //log method prints output,text() its not a cypress command its jquery method
+
+		})
+		
+		// cy.log(cy.get('.brand').text()) ....throws an error because its not a function
 	});
 });
+console.log('This will be printed in the web browser console');
---
+++ cypress/integration/examples/Test2.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+describe("My  Second Test Suite", function (){
+	it("My SecondTest case", function () {
+		cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+		cy.get(".search-keyword").type("ca");
+		cy.wait(2000);
+	
+		// Parent child chaining
+		cy.get('.products').as('productLocator')// This is aliasing 
+				
+		// Finds all four products and iterates thru the array using each($el, index, $list)
+		cy.get(".products").find(".product").each(($el, index, $list) => {
+			const textVeg = $el.find("h4.product-name").text();
+				if (textVeg.includes("Cashews")) {
+					cy.wrap($el).find('button').click()
+				}
+		});
+		
+	});
+});
+console.log('This will be printed in the web browser console');
