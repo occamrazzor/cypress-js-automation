64bf01ae0dad9c273ade4dcbf565c99e5b29d16c
occam_razor <occam_razor@hotmail.com>
2022-09-28 12:47:13 -0700

    Dynamic DropDown list selection

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -13,6 +13,15 @@ describe("My  Third Test Suite", function (){
 		//Static dropdowns select and pass it the option name or value attribue of the option 
 		cy.get('select').select('option2').should('have.value','option2')
 
+		// Dynamic dropdown Selection
+		cy.get('#autocomplete').type('ind')
+		cy.get('.ui-menu-item div').each(($el, index, $list)=>{
+			if ($el.text() === 'India') {
+				cy.wrap($el).click()
+			}
+		})
+
+		cy.get('#autocomplete').should('have.value', 'India')
 
 	});
 });
