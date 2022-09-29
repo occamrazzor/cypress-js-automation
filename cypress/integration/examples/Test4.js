9033fd1bcad2ac59f69d691976c9b4fd8fe28f21
occam_razor <occam_razor@hotmail.com>
2022-09-28 19:36:27 -0700

    invoke remove target open website  same tab

--- cypress/integration/examples/Test4.js
+++ cypress/integration/examples/Test4.js
@@ -1,10 +1,12 @@
 /// <reference types="Cypress" />
 
-const { invoke } = require("cypress/types/lodash");
+//const { invoke } = require("cypress/types/lodash"); <--- This line automatically appeared and broke the testcase after invoke.
+//was added in line 27. Diabling the offending line runs the test script without any issues.
 
 describe("My  Fourth Test Suite", function (){
 	it("My FourthTest case", function () {
 		cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+	
 		cy.get('#alertbtn').click()
 		cy.get('[value="Confirm"]').click() //Note Cypress autoaccepts alerts and pop-ups
 		//windows:alert trigger this event from cypress the pop up can be captured 
@@ -21,7 +23,14 @@ describe("My  Fourth Test Suite", function (){
 		})
 		// invoke method call a function and in jQuery removeattr function will remove the attribute of an element
 		// cypress supports all jquery objects and functions
-		cy.get('#opentab').invoke('removeAttr', 'target').click()
+		// Assertions include, be , have  and comparing two strings
+
+		cy.get('#opentab').invoke('removeAttr', 'target').click() // this will open the website in the same tab because default is new tab.
+		cy.url().should('include', 'rahulshettyacademy')
+
+		//browser navigation method
+		cy.go('back')
+		
 
 	});
 });
