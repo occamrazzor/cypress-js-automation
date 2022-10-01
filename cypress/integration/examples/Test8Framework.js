05e4362ecb17818def858c203bc2f39525392db8
occam_razor <occam_razor@hotmail.com>
2022-10-01 11:52:04 -0700

    Final commit before dev on POM branch

--- cypress/integration/examples/Test8Framework.js
+++ cypress/integration/examples/Test8Framework.js
@@ -9,7 +9,7 @@ describe("Framework Test Suite with Json data file in fixtures folder", function
         // The scope of data is restricted in the fixture block
         cy.fixture('example').then(function (data) {
             this.data=data
-
+            //globalThis.data=data
         })
     })
     
@@ -32,11 +32,39 @@ describe("Framework Test Suite with Json data file in fixtures folder", function
         // To verify if radio button is Entrepreneur (disabled) if its behaviour then use .should(be.  else if you are returning property then use .should(have.value) eg. Test3.js
         cy.get('#inlineRadio3').should('be.disabled')
 
+        //////////////////////////////////////////////////////////////
+        //	47. Building customized Cypress Commands for reusing the code.
+        //  Write function to choose product by name and add multiple products
+            //49. cy.pause() <-- this line will pause the cypress during execution. or append .debug()
+        cy.get(':nth-child(2) > .nav-link').click()
+        // Write a common locator to grab all the text to find desired text. eg Test5.js
 
+     /*    cy.get('h4.card-title').each(($el, index, $list) => {
+            if($el.text().includes('Blackberry')) {
+                cy.get('button.btn.btn-info').eq(index).click()
+            }    
+        }) */ // The code above is transffered into the support folder into a function that take productName
+        // Here we need to build a function to replication the above code with custom cypress methods in support folder
+        // The code below will work however, code on line 52 is parameterized with added code in example.json
+        /*  cy.selectProduct('Blackberry')
+            cy.selectProduct('Nokia Edge')  */
+         /////////////////////////////////////////////////////////////
+        //Need to pull up each and every value present in the example.json productName array and repeat the select product for each and every product present in the array by using javascript array syntax
+        /* const array1 = ['a', 'b', 'c'];
 
+        array1.forEach(element => console.log(element));
+        
+        // expected output: "a"
+        // expected output: "b"
+        // expected output: "c" */
+        
 
-         
-	
+        this.data.productName.forEach(function (element) {
+
+            cy.selectProduct(element)
+        });
+        // Final note everything is encapsulated from your custom commands (support/command.js) and the elements are derived from the fixtures(/fixtures/example.json)
+
     });
     
   
